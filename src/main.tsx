3414513211b56e0b43328cccf67b21879486fead
oxadzn <xensab3r@gmail.com>
2025-12-18 16:02:57 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Deprecated TypeScript entry; kept empty to avoid import errors.
